3b5f0a6fa1c5c42c8955599e7de7b28938fff16b
PHJUNITY <45454119+PHJUNITY@users.noreply.github.com>
2024-12-05 13:25:04 +0900

    Initial commit

---
+++ src/main/kotlin/teamhp/alarm_open/AlarmOpenApplication.kt
@@ -0,0 +1,11 @@
+package teamhp.alarm_open
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AlarmOpenApplication
+
+fun main(args: Array<String>) {
+    runApplication<AlarmOpenApplication>(*args)
+}
---
+++ src/test/kotlin/teamhp/alarm_open/AlarmOpenApplicationTests.kt
@@ -0,0 +1,13 @@
+package teamhp.alarm_open
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AlarmOpenApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
